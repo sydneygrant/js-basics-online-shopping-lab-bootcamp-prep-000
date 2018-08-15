e5fc457c94be6dcedcd19cb9a5d46557082d357a
sydneygrant <sgrant@nyu.edu>
2018-08-15 21:37:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,26 @@ function addToCart(item) {
 
 }
 
+`In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+    + ***Note***: Pay close attention to the syntax above. The returned statement should be a single
+    sentence that begins with `In your cart, you have `, terminates in a period, and can assume the following shapes
+    according to how many items the cart contains:
+      * 1 item — `In your cart, you have bananas at $17.`
+      * 2 items — `In your cart, you have bananas at $17, and pancake batter at $5.`
+      * 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+
 function viewCart() {
   // write your code here
+  var str = "In your cart, you have ";
+  if (cart.length == 0){
+    return "Your shopping cart is empty.";
+  } else if (cart.length == 1){
+    str += `${cart[0]} at ${cart[0][0]}`
+  } else {
+    for (var i = 0; i < cart.length; i++){
+      
+    }
+  }
 }
 
 function total() {
