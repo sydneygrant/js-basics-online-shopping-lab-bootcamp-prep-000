55788e27a2ebae90d3e0280bfafc4887fc174b7a
sydneygrant <sgrant@nyu.edu>
2018-08-16 02:46:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,24 +29,49 @@ function addToCart(item) {
       * 2 items — `In your cart, you have bananas at $17, and pancake batter at $5.`
       * 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
 
-function viewCart() {
-  // write your code here
-  var str = "In your cart, you have ";
-  if (cart.length == 0){
-    return "Your shopping cart is empty.";
-  } else if (cart.length == 1){
-    str += `${cart[0].key} at ${cart[0].value}.`;
-  } else if (cart.length == 2){
-    str += `${cart[0].key} at ${cart[0].value}, and ${cart[1].key} at ${cart[1].value}.`;
-  } else {
-    for (var i = 0; i < cart.length; i++){
-      str += `${cart[i].key} at ${cart[i].value}, and`;
-      if (i == cart.length-1){
-        str += '.';
-      }
-    }
-  } return str;
-}
+// function viewCart() {
+//   // write your code here
+//   var str = "In your cart, you have ";
+//   if (cart.length == 0){
+//     return "Your shopping cart is empty.";
+//   } else if (cart.length == 1){
+//     str += `${cart[0].key} at ${cart[0].value}.`;
+//   } else if (cart.length == 2){
+//     str += `${cart[0].key} at ${cart[0].value}, and ${cart[1].key} at ${cart[1].value}.`;
+//   } else {
+//     for (var i = 0; i < cart.length; i++){
+//       str += `${cart[i].key} at ${cart[i].value}, and`;
+//       if (i == cart.length-1){
+//         str += '.';
+//       }
+//     }
+//   } return str;
+// }
+
+function viewCart (){
+ if(cart.length === 0){
+   return 'Your shopping cart is empty.';
+ }
+ else{
+       var myString = "In your cart, you have "
+
+         for(var i = 0, l = cart.length; i < l; i++){
+             for(var list in cart[i]){
+             myString+=`${list} at $${cart[i][list]}`
+                 if(i!==cart.length-1){
+                   myString+=", "
+                 }
+                 else{
+                   myString+="."
+                 }
+               }
+
+             }
+
+       return myString;
+   }
+
+ }
 
 function total() {
   // write your code here
